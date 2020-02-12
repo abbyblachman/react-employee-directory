c7d8206b3e24481e0a6f631266d0a2bab7170d26
Abby <abbysblachman@gmail.com>
2020-02-12 10:53:56 -0600

    working with styling

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Table from "../Table/Table"
+import { black } from "color-name";
+
+const style = {
+  body: {
+    background: "black" 
+  },
+  fontColor: {
+    color: "white"
+  }
+}
 
 
 function Container() {
   return (
-    <body className = "py-4">
-      <div className="container">
+    <body className = "py-4" style={style.body}>
+      <div className="container" style={style.fontColor}>
         <h1>Employee Directory</h1>
         <p className="lead">A fast and easy way to search for employees.</p>
         <p>You can filter the employee directory to see specific results.</p>
