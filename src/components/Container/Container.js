f79be8a023f1ba9f7c5c31467fb83eef5f30a190
abbyblachman <abbysblachman@gmail.com>
2020-04-06 09:52:02 -0500

    getting rid of unecessary comments

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,23 +1,10 @@
 import React from "react";
 
-// const style = {
-//   body: {
-//     background: "black" 
-//   },
-//   fontColor: {
-//     color: "white"
-//   }
-// }
-
-
 function Container(props) {
   return (
     <div className="py-4">
       <div className="container">
         {props.children}
-        {/* <h1>Employee Directory</h1>
-        <p className="lead">A fast and easy way to search for employees.</p>
-        <p>You can filter the employee directory to see specific results.</p> */}
       </div>
       </div>
 
